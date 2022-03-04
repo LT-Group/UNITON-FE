50fb4c4645a37778f47805698bd89885765c91b3
Sunjae95 <tjswo9502@gmail.com>
2022-03-05 02:53:25 +0900

    Chore: next, emotion, mui, recoil초기설정

---
+++ pages/_app.jsx
@@ -0,0 +1,15 @@
+import { CssBaseline } from '@mui/material';
+import { RecoilRoot } from 'recoil';
+
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <>
+      <RecoilRoot>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </RecoilRoot>
+    </>
+  );
+};
+
+export default MyApp;
