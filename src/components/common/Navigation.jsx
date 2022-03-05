2a36103b59b6567b3276949f0fe7cb8de3eb0260
변우진 <sforot9@gmail.com>
2022-03-05 18:57:50 +0900

    feat: Navigation router

--- src/components/common/Navigation.jsx
+++ src/components/common/Navigation.jsx
@@ -1,17 +1,24 @@
+import React, { useState } from 'react';
 import { BottomNavigation, BottomNavigationAction } from '@mui/material';
-import { useState } from 'react';
-import theme from '../../styles';
 import { MypageIcon, RankingIcon, SolveIcon } from './Icon';
-
+import { useRouter } from 'next/router';
 const Navigation = () => {
+  const router = useRouter();
   const [value, setValue] = useState('solve');
   return (
     <BottomNavigation
       showLabels
+      sx={{
+        width: '100%',
+        position: 'fixed',
+        bottom: 0,
+        boxShadow: '0 0 0.15rem 0.15rem rgba(0,0,0,0.05)',
+      }}
       value={value}
       color="success"
       onChange={(event, newValue) => {
         setValue(newValue);
+        router.replace(`/${newValue}`);
       }}
     >
       <BottomNavigationAction
