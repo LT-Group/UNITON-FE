8bc84b9187075e08dbfe408bb4597c70da5e678c
변우진 <sforot9@gmail.com>
2022-03-05 15:00:39 +0900

    fix: Container background

--- src/components/common/Contianer.jsx
+++ src/components/common/Contianer.jsx
@@ -1,14 +1,18 @@
 import styled from '@emotion/styled';
+import PropTypes from 'prop-types';
 
 const Container = styled.div`
   width: 100%;
   height: 100vh;
   padding: 2.4rem;
   padding-bottom: 0;
-
+  background-color: ${(props) => (props?.bgColor ? props?.bgColor : 'none')};
   display: flex;
   flex-direction: column;
   align-items: center;
 `;
-
+Container.propTypes = {
+  //객체를 가진 배열을 proptype로 받음
+  bgColor: PropTypes.string,
+};
 export default Container;
