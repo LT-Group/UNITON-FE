4092aac9af4971bae35a94f795be8092857dd586
변우진 <sforot9@gmail.com>
2022-03-05 14:11:36 +0900

    feat: container components

---
+++ src/components/common/Contianer.jsx
@@ -0,0 +1,14 @@
+import styled from '@emotion/styled';
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  padding: 2.4rem;
+  padding-bottom: 0;
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+export default Container;
