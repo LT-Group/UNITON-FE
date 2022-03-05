32a9d76f525e52b336f4b9a734699e87426e287d
변우진 <sforot9@gmail.com>
2022-03-06 06:01:55 +0900

    fix: isLogin redirect

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -47,7 +47,8 @@ const MyApp = ({ Component, pageProps }) => {
         // 메인 페이지가 아닌 경우 -> 메인페이지로 이동
         if (
           router.router.asPath !== '/login' &&
-          router.router.asPath !== '/signup'
+          router.router.asPath !== '/signup' &&
+          router.router.asPath !== '/mypage'
         )
           router.replace('/');
       } else if (isLogin) {
